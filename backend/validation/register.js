f188969bf5d64b05c16226f577b2bfa3c6234c7b
ayeshNipun <nipun.yesh@gmail.com>
2019-06-05 10:31:49 +0530

    validation restart

--- backend/validation/register.js
+++ backend/validation/register.js
@@ -4,15 +4,14 @@ const Validator = require('validator');
 //files
 const isEmpty = require('./is-empty');
 
-//input validation function
 module.exports = function validateRegisterInput(data) {
 	let errors = {};
-
-	if (!Validator.isLength(data.name, { min:2, max:30 })) {
-		errors.name = 'Name must be between 2 and 30 charcters'
+	
+	if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+		errors.name = 'Name mst be between 2 and 30 characters'
 	}
 
-	return{
+	return {
 		errors,
 		isValid: isEmpty(errors)
 	}
