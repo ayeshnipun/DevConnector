9bbe7b2ba27e8915d86c071a1e98c6c5f84759fb
ayeshNipun <nipun.yesh@gmail.com>
2019-06-15 21:11:29 +0530

    get current user route

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,14 +1,28 @@
 //packages
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const mongoose = require("mongoose")
+
+//models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route 	GET /api/profile
-// @desc 	Test profile route
-// @access 	Public
-router.get('/', (req, res) => {
-	res.json({
-		msg: 'Profile Works'
-	})
+// @desc 	Get current user profile
+// @access 	Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const errors = {};
+
+	Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = "There is no profile found for this user"
+				return res.status(404).json(errors);
+			}
+			res.json(profile)
+		})
+		.catch(err => res.json(err));
 })
 
 module.exports = router;
